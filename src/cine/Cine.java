9797a07f97e020d0c127c40065fb32e3360721ab
Pablo B <PabloB94@users.noreply.github.com>
2017-04-14 23:33:56 +0200

    Update Cine.java
    Faltan los 3 métodos que hacías tú.

--- src/cine/Cine.java
+++ src/cine/Cine.java
@@ -16,11 +16,11 @@ public class Cine {
     }
 
     public void comprarEntrada (int sala, int sesion, int fila, int columna){
-
+    	this.sala[sala].comprarEntrada(sesion, fila, columna);
     }
 
     public int getIdEntrada (int sala, int sesion, int fila, int columna){
-
+    	return this.sala[sala].getIdEntrada(sesion, fila, columna);
     }
 
     public char[] getEstadoSesion (int sala, int sesion){
@@ -32,27 +32,27 @@ public class Cine {
     }
 
     public String[] getHorasDeSesionesDeSala (int sala){
-
+    	return this.sala[sala].getHorasDeSesionesDeSala();
     }
 
     public String recogerEntradas (int id, int sala, int sesion){
-
+    	return this.sala[sala].recogerEntradas(id, sesion);
     }
 
     public int getButacasDisponiblesSesion (int sala, int sesion){
-
+    	return this.sala[sala].getButacasDisponiblesSesion(sesion) ;
     }
 
     public ButacasContiguas recomendarButacasContiguas (int noButacas, int sala, int sesion){
-
+    	return this.sala[sala].recomendarButacasContiguas(noButacas, sesion);
     }
 
     public void comprarEntradasRecomendadas (int sala, int sesion, ButacasContiguas butacas){
-
+    	this.sala[sala].comprarEntradasRecomendadas(sesion, butacas);
     }
 
     public void incluirSesion (int sala, String horaSesion){
-
+    	this.sala[sala].incluirSesion(horaSesion);
     }
 
     public void borrarSesion (int sala, String horaSesion){
